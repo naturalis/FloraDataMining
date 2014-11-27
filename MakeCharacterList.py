9b683d90898d00a885686f5bc50a7f39678ffb79
HesterS <hmstekelenburg@gmail.com>
2014-11-27 09:13:55 +0100

    Commit  message

--- MakeCharacterList.py
+++ MakeCharacterList.py
@@ -1,16 +1,15 @@
+import xml.dom.minidom
 import xml.etree.ElementTree as ET
 tree = ET.parse("A24_final.xml")
-root = tree .getroot()
+root = tree.getroot()
+path = "./treatment/taxon/feature//"
 
-treatment = root[1]
-taxon = treatment[0]
-feature = taxon[2]
-
-for taxon in treatment:
-	print treatment.attrib
-
-#taxon = feature[0]
-#for feature in root.findall("./treatment/taxon"): 
-#	print feature.attrib
-
+for character in root.findall(path):
+	output = str(character.attrib)
+	if character.hasChildNodes():
+		
+		if character.tag == "char" or character.tag == "subChar":
+		children = character.getchildren()
+		for child in children:
+			print str(character.attrib) + "/"+ str(child.attrib) 
 
