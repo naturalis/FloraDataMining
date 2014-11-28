3207f67a938aced2043cadfb8edc9449bbe5f126
HesterS <hmstekelenburg@gmail.com>
2014-11-28 09:51:49 +0100

    Commit message

--- MakeCharacterList.py
+++ MakeCharacterList.py
@@ -1,15 +1,18 @@
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
-tree = ET.parse("A24_final.xml")
+tree = ET.parse("A24_Piperaceae.xml")
 root = tree.getroot()
-path = "./treatment/taxon/feature//"
 
-for character in root.findall(path):
-	output = str(character.attrib)
-	if character.hasChildNodes():
-		
-		if character.tag == "char" or character.tag == "subChar":
-		children = character.getchildren()
-		for child in children:
-			print str(character.attrib) + "/"+ str(child.attrib) 
+def constructHierarchy(character, path):
+	path = path + "/" + character.get('class')
+	print path
+	if character.getchildren():
+		for child in character.getchildren():		
+			if child.get('class') != None:
+				constructHierarchy(child, path)
 
+			
+for feature in root.findall("./treatment/taxon/feature"):
+	for child in feature.getchildren():
+		if child.get('class') != None:
+			constructHierarchy(child, "")
