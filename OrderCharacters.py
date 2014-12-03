e81df06271603ad0591ed9b87f5317fdc6df5468
HesterS <hmstekelenburg@gmail.com>
2014-12-03 16:28:54 +0100

    Commit message

--- OrderCharacters.py
+++ OrderCharacters.py
@@ -4,34 +4,53 @@
 import sys
 import xml.etree.ElementTree as ET
 
-tree = ET.parse(sys.argv[1])
+tree = ET.parse("A24_Piperaceae.xml")
+characters = open("selection_of_characters_Pip.csv", "r")
 root = tree.getroot()
-out = open("Flora_Matrix.txt", "w") 
-numberOfFeatures = 1
+numberOfFeatures = 0
 
 
-def printToCsv(matrix):
-	line = ""
-	for i in range( len(matrix)):
-		for j in range( len(matrix[i] )):
+def printToCsv(matrix):	
+	for i in range(len(matrix)):
+		line = ""
+		for j in range(len(matrix[0])):
 			line = line + str(matrix[i][j]) + ","	
+		print line 
+
+
+def getCharacters(matrix, chars):
+	for line in chars:
 		print line
+		char = line.split(",")[0]
+		print char
+		if line.split(",")[2] == "Y":
+			matrix[0][0] = char
 
 
 def getSpecies(matrix):	
-	i = 0	
-	for nomenclature in root.findall("./treatment/taxon/nomenclature"):
-		
+	i = 0
+	for nomenclature in root.findall("./treatment/taxon/nomenclature"):		
 		homotypes = nomenclature[0]
-		nametype = homotypes.find('nameType')
-		print nametype.tag
-		if homotypes.get('class') == 'accepted':
-			i = i + 1
-			genus = nomenclature[1][0].text
-			species = nomeclature[1][1].text
-			matrix[0][i] = genus + " " + species	
+		if homotypes[0].get('class') == 'accepted':
+			i+=1
+			for name in homotypes.findall('.//name'):
 				
-	
+				if name.get('class') == 'genus':
+					genus = name.text
+				elif name.get('class') == 'species':
+					species = name.text
+				
+			matrix[i][0] = genus + " " + species
+			
+
+def getNumberOfCharacters(char):
+	numberOfChar = 0
+	for line in char:
+		if line.split(",")[2] == "Y":
+			numberOfChar+=1
+	return numberOfChar
+
+
 def getNumberOfSpecies():
 	numberOfSpecies = 0
 	for homotype in root.findall("./treatment/taxon/nomenclature/homotypes"):		
@@ -41,10 +60,12 @@ def getNumberOfSpecies():
 	return numberOfSpecies
 
 
+numberOfCharacters = getNumberOfCharacters(characters)
 numberOfSpecies = getNumberOfSpecies()
-matrix = [[0 for i in range(numberOfSpecies + 1)] for j in range(numberOfFeatures + 1)]
+matrix = [[0 for i in range(numberOfFeatures + 1)] for j in range(numberOfSpecies + 1)]
 getSpecies(matrix)
-printToCsv(matrix)				
+getCharacters(matrix, characters)
+				
 					
 
 					
