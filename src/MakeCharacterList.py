78aa9b99c561dc9abb401a232e7b70add9b30601
HesterS <hmstekelenburg@gmail.com>
2014-12-10 16:33:38 +0100

    Commit message

---
+++ src/MakeCharacterList.py
@@ -0,0 +1,18 @@
+import xml.dom.minidom
+import xml.etree.ElementTree as ET
+tree = ET.parse("A24_Piperaceae.xml")
+root = tree.getroot()
+
+def constructHierarchy(character, path):
+	path = path + "/" + character.get('class')
+	print path
+	if character.getchildren():
+		for child in character.getchildren():		
+			if child.get('class') != None:
+				constructHierarchy(child, path)
+
+			
+for feature in root.findall("./treatment/taxon/feature"):
+	for child in feature.getchildren():
+		if child.get('class') != None:
+			constructHierarchy(child, "")
