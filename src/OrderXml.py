063679b34fe7a33b8f3b528f286b22e9e8088ba5
HesterS <hmstekelenburg@gmail.com>
2014-12-16 15:03:44 +0100

    The current matrix is in the file Piperaceae.tsv. I made a new xml file with some changesin the text: Piperaceae.xml. The reason was to correct the text, to let it be displayed in a matrix.

--- src/OrderXml.py
+++ src/OrderXml.py
@@ -6,10 +6,10 @@ xmlFile = open(sys.argv[1], 'r')
 output = open("ordened.xml", 'w')
 
 for line in xmlFile:
-	fields = re.split(r'[<>]', line)
-	if fields[1][0:4] == "char" and len(fields) > 6:
-		print line		
-		fields[2] = fields[2] + fields[6]
+	if re.search('<\/subChar>, [a-z]', line) or re.search('<\/subChar>: [a-z]', line):
+		print line
+		fields = re.split(r'[<>]', line)		
+		fields[2] = fields[2] + "|" + fields[6]
 		line = fields[0] + "<"
 
 		for i in range(1, len(fields) - 1):
@@ -17,5 +17,5 @@ for line in xmlFile:
 				line += fields[i] + "<"
 			else:
 				line += fields[i] + ">"
-		print line					
+		print line		
 	output.write(line)
